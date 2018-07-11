8bd8aaf3e0d932bfb328f0fea8e84c7ff569451c
pardhasm <pardhasaradhi108@gmail.com>
2018-07-12 01:30:14 +0530

    tech debt

--- src/main/java/com/pardhasm/sieve/APIDefinition.java
+++ src/main/java/com/pardhasm/sieve/APIDefinition.java
@@ -5,7 +5,6 @@ import io.undertow.server.handlers.proxy.ProxyHandler;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -72,8 +71,8 @@ public class APIDefinition {
         return name;
     }
 
-    public ArrayList<Target> getTargets() {
-        return (ArrayList<Target>) targets;
+    public List<Target> getTargets() {
+        return targets;
     }
 
     public Long getGlobalRateLimit() {
--- src/main/java/com/pardhasm/sieve/Constants.java
+++ src/main/java/com/pardhasm/sieve/Constants.java
@@ -2,6 +2,9 @@ package com.pardhasm.sieve;
 
 public final class Constants {
 
+    private Constants() {
+    }
+
     public static final String CONFIG_FOLDER = "config";
     public static final String ADDRESS = "://";
     public static final String COLON = ":";
